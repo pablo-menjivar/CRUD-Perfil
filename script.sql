46f867f8adeaa185ebe7f5f5a92784e44e1531e2
Pablo <20200135@ricaldone.edu.sv>
2024-06-04 15:38:17 -0600

    First commit

---
+++ script.sql
@@ -0,0 +1,10 @@
+create table tbTickets (
+numero_ticket number(10) primary key,
+titulo_ticket varchar2(50) not null,
+descripcion_ticket varchar2(200) not null,
+autor_ticket varchar2(40) not null,
+email_autor varchar2(100) not null,
+creacion_ticket date not null,
+estado_ticket varchar(20) check (estado_ticket in ('Activo', 'Finalizado')) not null,
+finalizacion_ticket date not null
+)
